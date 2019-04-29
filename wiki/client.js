bed81532962061faab443ec3172f41fd859a920a
felipGonzalez <afelipgb@gmail.com>
2019-04-29 18:13:28 -0500

    segundo commit

--- wiki/client.js
+++ wiki/client.js
@@ -3,10 +3,8 @@ angular.module('demo',[])
 	var tiempo1 = new Date()
 	var m1 = tiempo1.getTime()
 	var s;
-	$http.get('http://localhost:3000/text').
+	$http.get('http://localhost:3000').
 	then(function(response) {
-		var text = response.data
-		console.log(text)
 		$scope.texto = response.data;
 		var tiempo2 = new Date()
 		$scope.time = tiempo2.getTime() - m1
